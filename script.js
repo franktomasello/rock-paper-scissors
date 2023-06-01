14c2c0033a9a08e6ba0edd28efd1db45c5e99878
franktomasello <franktomasello@outlook.com>
2023-05-31 17:23:02 -0700

    created the getComputerChoice function and index

---
+++ script.js
@@ -0,0 +1,17 @@
+function getComputerChoice() {
+    //declaring an array literal
+    const rockPaperScissors = [];
+
+    //pushing the elements to the array
+    rockPaperScissors.push("Rock");
+    rockPaperScissors.push("Scissors");
+    rockPaperScissors.push("Paper");
+
+    //pick a random index
+    const randomIndex = Math.floor(Math.random() * rockPaperScissors.length);
+    //choose a random element from the array
+    const randomElement = rockPaperScissors[randomIndex];
+
+    //return the randomized element from the array
+    return randomElement;
+}
