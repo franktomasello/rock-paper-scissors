97d2c31b443a27ce2f2557afb3490b10a9c6bc82
franktomasello <franktomasello@outlook.com>
2023-06-07 23:45:55 -0700

    fdccd

--- script.js
+++ script.js
@@ -44,3 +44,8 @@ const computerSelection = getComputerChoice();
 console.log(playARound(playerSelection, computerSelection));
   
 
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playARound(playerSelection, computerSelection)
+    }
+}
