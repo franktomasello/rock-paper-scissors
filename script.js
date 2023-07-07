904f1c49c5040059b5d659393f1c0abe22acaed1
franktomasello <franktomasello@outlook.com>
2023-07-07 14:48:17 -0700

    made necessary changes to the UI

--- script.js
+++ script.js
@@ -1,83 +1,72 @@
-function getComputerChoice() {
-  // Declaring an array literal
-  const rockPaperScissors = ["Rock", "Scissors", "Paper"];
+const ROCK_PAPER_SCISSORS = ["rock", "scissors", "paper"];
 
-  // Pick a random index
-  const randomIndex = Math.floor(Math.random() * rockPaperScissors.length);
-
-  // Return the randomized element from the array
-  return rockPaperScissors[randomIndex];
+function getRandomChoice() {
+  const randomIndex = Math.floor(Math.random() * ROCK_PAPER_SCISSORS.length);
+  return ROCK_PAPER_SCISSORS[randomIndex];
 }
 
-function playARound(playerSelection, computerSelection) {
-  // Convert both selections to lowercase for case-insensitive comparison
-  playerSelection = playerSelection.toLowerCase();
-  computerSelection = computerSelection.toLowerCase();
-
-  // Check for a tie
+function getRoundResult(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     return "It's a tie!";
   }
-
-  // Check for player win conditions
-  if (
-    (playerSelection === 'rock' && computerSelection === 'scissors') ||
-    (playerSelection === 'paper' && computerSelection === 'rock') ||
-    (playerSelection === 'scissors' && computerSelection === 'paper')
-  ) {
-    return "You win!";
-  }
-
-  // If none of the above conditions are met, the player loses
-  return "You lose!";
+  const winConditions = {
+    rock: 'scissors',
+    paper: 'rock',
+    scissors: 'paper'
+  };
+  return winConditions[playerSelection] === computerSelection ? "You win!" : "You lose!";
 }
 
-document.addEventListener("DOMContentLoaded", function(){
-  // Select all buttons
-  let buttons = document.querySelectorAll('button');
+function updateScores(result, playerScore, computerScore) {
+  if (result === "You win!") {
+    playerScore++;
+  } else if (result === "You lose!") {
+    computerScore++;
+  }
+  return [playerScore, computerScore];
+}
 
-  // Select results div
-  let resultsDiv = document.querySelector('#results');
+function checkGameEnd(playerScore, computerScore) {
+  if (playerScore === 5) {
+    return "Player wins the game!";
+  } else if (computerScore === 5) {
+    return "Computer wins the game!";
+  }
+  return null;
+}
 
-  // Select score divs
-  let playerScoreDiv = document.querySelector('#playerScore');
-  let computerScoreDiv = document.querySelector('#computerScore');
-
-  // Initialize scores
+document.addEventListener("DOMContentLoaded", function() {
+  const buttons = document.querySelectorAll('button');
+  const resultsDiv = document.querySelector('#results');
+  const playerScoreDiv = document.querySelector('#playerScore');
+  const computerScoreDiv = document.querySelector('#computerScore');
   let playerScore = 0;
   let computerScore = 0;
 
-  // Add event listener to each button
   buttons.forEach((button) => {
     button.addEventListener('click', function() {
-      // Get playerSelection from button text
-      let playerSelection = button.innerText;
-      let computerSelection = getComputerChoice();
+      const playerSelection = button.innerText.toLowerCase();
+      const computerSelection = getRandomChoice();
+      const result = getRoundResult(playerSelection, computerSelection);
+      [playerScore, computerScore] = updateScores(result, playerScore, computerScore);
 
-      // Run playARound function with playerSelection and computerSelection
-      let result = playARound(playerSelection, computerSelection);
-
-      // Update scores based on the result
-      if (result === "You win!") {
-        playerScore++;
-      } else if (result === "You lose!") {
-        computerScore++;
-      }
-
-      // Display result in the results div
-      resultsDiv.innerText = result;
-
-      // Display scores
+      // Update the score displays
       playerScoreDiv.innerText = "Player Score: " + playerScore;
       computerScoreDiv.innerText = "Computer Score: " + computerScore;
-
-      // Check if either score has reached 5 and display winner
-      if (playerScore === 5) {
-        resultsDiv.innerText += "\nPlayer wins the game!";
-      } else if (computerScore === 5) {
-        resultsDiv.innerText += "\nComputer wins the game!";
+      
+      const gameEndMessage = checkGameEnd(playerScore, computerScore);
+      if (gameEndMessage) {
+        resultsDiv.innerText = gameEndMessage;
+        playerScore = 0;
+        computerScore = 0;
+      } else {
+        resultsDiv.innerText = result;
       }
     });
   });
 });
+
+
+
+
 
